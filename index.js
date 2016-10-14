d5beba71f4bca71c38404b02d4bccb481ce2e92c
Ersin Buckley <ersin@mediasuite.co.nz>
2016-10-14 09:17:12 +0800

    add proj4js, update js so we request bbox information for a click on the map

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ const app = express()
 const url = require('url')
 const path = require('path')
  
-app.use('/geoserver/wms', proxy('http://localhost:8080/geoserver/wms', {
+const geoserverProxy = {
   forwardPath: (req, res) => {
     return url.parse(req.originalUrl).path
   }
-}))
+}
+
+app.use('/geoserver/wms', proxy('http://localhost:8080/geoserver/wms', geoserverProxy))
+app.use('/geoserver/wfs', proxy('http://localhost:8080/geoserver/wfs', geoserverProxy))
 
 app.use('/static', express.static('static'))
 
--- static/script.js
+++ static/script.js
@@ -1,22 +1,7 @@
-function asyncXML (url) {
-  var xhttp = new XMLHttpRequest()
-  return new Promise((resolve, reject) => {
-    xhttp.onreadystatechange = function() {
-      if (this.readyState == 4) {
-        if (this.status == 200) resolve(this)
-        else reject(this)
-      }
-    }
-    xhttp.open("GET", url, true);
-    xhttp.send()
-  })
-}
-    
 function getLayers () {
   return document.getElementById('layer-input').value
 }
 
-function noOp () {}
 
 const loaded = new Promise((resolve, reject) => window.addEventListener('load', event => resolve(event)))
 
@@ -26,9 +11,20 @@ const makeSelectOption = ({name, title}) => {
   option.innerHTML = title
   return option
 }
+const zoomedOrMoved = (map, cb) => {
+  map.on('zoomend', cb)
+  map.on('moveend', cb)
+}
+
+function setMapViewFromUrl(map, url) {
+  const lat = url.searchParams.get('lat') || -36.85
+  const lng = url.searchParams.get('lng') || 174.76
+  const zoomlevel = url.searchParams.get('z') || 10
+  return map.setView(L.latLng(lat, lng), zoomlevel)
+}
 
 loaded
-.then(_ => asyncXML('/geoserver/wms?service=wms&request=GetCapabilities&version=1.3.0'))
+.then(_ => xhrGET('/geoserver/wms?service=wms&request=GetCapabilities&version=1.3.0'))
 .then(result => {
   const layers = result.responseXML.querySelectorAll("Layer[queryable=\"1\"]")
   let model = []
@@ -63,19 +59,100 @@ loaded
   })
 
   const map = L.map('my-map', {
-    center: L.latLng(36.85, 174.76),
+    center: L.latLng(-36.85, 174.76),
     zoom: 10,
     continuousWorld: true,
     worldCopyJump: false,
   })
-  map.setView(L.latLng(-36.85, 174.76), 10)
+
+  setMapViewFromUrl(map, new URL(window.location.href))
+
+  zoomedOrMoved(map, e => {
+    const location = map.getCenter()
+    const zoomlevel = map.getZoom()
+    let url = new URL(window.location.href)
+    url.searchParams.set('z', zoomlevel)
+    url.searchParams.set('lat', location.lat)
+    url.searchParams.set('lng', location.lng)
+    
+    window.history.pushState({path: url.href}, '', url.href);
+  })
+
   map.on('click', e => {
     console.log('dat click in', e)
+    query(e.latlng.lat, e.latlng.lng)
+      .then(yo => {
+        console.log('got data for point', e.latlng, yo)
+      })
+      .catch(err => {
+        console.log('bad request', err)
+      })
   })
   map.addLayer(tileLayer)
 
   loadButton.addEventListener('click', e => {
-    console.log('clicked load button')
     tileLayer.setParams({layers: getLayers()})
   })
+  
+  window.addEventListener('popstate', e => {
+    const url = new URL(e.state.path)
+    setMapViewFromUrl(map, url)
+  })
 })
+
+// utillity
+function xhr (method, url, body) {
+  var xhttp = new XMLHttpRequest()
+  return new Promise((resolve, reject) => {
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4) {
+        if (this.status == 200) resolve(this)
+        else reject(this)
+      }
+    }
+    xhttp.open(method, url, true);
+    if (method === 'POST') {
+      xhttp.setRequestHeader("content-type", "application/xml")
+      xhttp.send(body)
+    } else {
+      xhttp.send()
+    }
+  })
+}
+function xhrGET (url) {
+  return xhr('GET', url)
+}
+
+function xhrPOST (url, body) {
+  return xhr('POST', url, body)
+}
+    
+function noOp () {}
+
+
+function query (lat, lng) {
+  var bboxQuery = `
+  <wfs:GetFeature
+    xmlns:ogc="http://www.opengis.net/ogc"
+    xmlns:gml="http://www.opengis.net/gml"
+    xmlns:wfs="http://www.opengis.net/wfs"
+    service="WFS"
+    version="1.1.0"
+    maxFeatures="10"
+    outputFormat="text/xml; subtype=gml/3.1.1">
+    <wfs:Query
+      srsName="EPSG:4326" typeName="ersin-map:parcels-reprojected">
+      <ogc:Filter>
+        <ogc:BBOX>
+          <ogc:PropertyName>shape</ogc:PropertyName>
+          <gml:Envelope srsName="EPSG:4326">
+            <gml:lowerCorner>${lng - 0.05} ${lat - 0.05}</gml:lowerCorner>
+            <gml:upperCorner>${lng + 0.05}  ${lat + 0.05}</gml:upperCorner>
+          </gml:Envelope>
+        </ogc:BBOX>
+      </ogc:Filter>
+    </wfs:Query>
+  </wfs:GetFeature>`
+  console.log(bboxQuery)
+  return xhrPOST("/geoserver/wfs", bboxQuery)
+}
